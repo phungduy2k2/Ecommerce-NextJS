afa8bf7455d1d52ce1e61f7c0ee352ff7cbaad6e
duy <phungvanduy10a4@gmail.com>
2024-09-26 00:42:48 +0700

    product detail page

--- src/services/product/index.js
+++ src/services/product/index.js
@@ -85,6 +85,19 @@ export const productByCategory = async (id) => {
     return data;
   }catch(e){
     console.log(e);
-    
+  }
+}
+
+export const productById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/admin/product-by-id?id=${id}`, {
+      method: 'GET',
+      cache: 'no-store',
+    })
+
+    const data = await res.json();
+    return data;
+  } catch(e){
+    console.log(e);
   }
 }
